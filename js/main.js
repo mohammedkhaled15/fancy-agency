7cd334101351c9c271fe452e88b972271270b4ce
mohammedkhaled15 <52936327+mohammedkhaled15@users.noreply.github.com>
2021-12-22 22:22:21 +0200

    ..

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let prog = document.querySelectorAll(".progress span")  
+let section = document.querySelector(".skills")
+
+window.onload = function(){
+
+    prog.forEach((a)=>a.style.width = "0")
+
+}
+
+window.onscroll = function(){
+
+    if(window.scrollY >= section.offsetTop - 100){
+
+        prog.forEach((a)=>a.style.width = a.dataset.width)
+    }
+}
